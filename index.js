98b9820f68bca9768231331b39292bf8f9f6d0e0
lancetoledo <lanceadrian13@gmail.com>
2021-04-14 22:19:05 -0400

    1 file modified

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ Compare current value to all the other values in the array
 let arr = [33, 21, 14, 3, 21, 21, 334, 579, 34, 565, 4345, 3987, 3];
 let cleanArr = [];
 
-
 function removeDupe(array) {
   //Iterate through the array
   for (let i = 0; i < array.length; i++) {
